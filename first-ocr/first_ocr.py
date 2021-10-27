937a8a70907503860e8f957b6151edee838d46e6
walaeddine <walaeddine.bourahla@outlook.com>
2021-10-27 22:09:27 +0800

    first ocr implemented

---
+++ first-ocr/first_ocr.py
@@ -0,0 +1,18 @@
+# import the needed packages
+import pytesseract
+import argparse
+import cv2
+
+# construct the argument parser and parse arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+                help="Path to the input image to be OCR's")
+args = vars(ap.parse_args())
+
+# load the input image and convert it from BGR to RGB channel ordering
+image = cv2.imread(args["image"])
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# use Tesseract to OCR the image
+text = pytesseract.image_to_string(image)
+print(text)
